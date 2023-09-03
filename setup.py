ba2de7e7f93f75e30e5c696c4e4e79eca4d1a725
sun0222 <sunnyspace23@gmail.com>
2023-09-03 17:21:26 +0530

    first setup commit

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+from typing import List
+
+def get_requirements(file_path:str)->List[str]:
+    """
+    It is return the list of requirements
+    """
+    requirements=[]
+    with open(file_path) as f:
+        requirements=f.readlines()
+        requirements=[req.replace("\n", "") for req in requirements]
+    return requirements
+
+
+
+
+setup(
+    name="Sunilkumar",
+    version="0.0.1",
+    author="Sunil",
+    author_email="sunilkumart461@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+
+)
